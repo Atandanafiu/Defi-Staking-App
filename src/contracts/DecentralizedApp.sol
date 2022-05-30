21bf46299e27e1553b393a0581df2c605ea8378a
Atanda Nafiu <atandanafiu21@gmail.com>
2022-05-30 04:12:47 +0100

    update

--- src/contracts/DecentralizedApp.sol
+++ src/contracts/DecentralizedApp.sol
@@ -7,4 +7,55 @@ contract Tether {
     string public symbol = "USDT";
     uint256 public supply = 1000000000000000000000000;
     uint8 public decimal = 18;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(address indexed _owner, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceof;
+
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor() {
+        balanceof[msg.sender] = supply;
+    }
+
+    function tranfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // Check if balanceOf is equal or greater than _value
+        require(balanceof[msg.sender] >= _value);
+        // transfer the amount and subtract the balance
+        balanceof[msg.sender] -= _value;
+        // add the balance
+        balanceof[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value >= balanceof[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        // add balance for transfrFrom
+        balanceof[_to] += _value;
+        // subract balance from transferFrom
+        balanceof[_from] -= _value;
+        allowance[msg.sender][_from] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
 }
--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -1,27 +1,19 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Migrations {
-    address public owner;
-    uint256 public last_completed_migration;
+  address public owner = msg.sender;
+  uint public last_completed_migration;
 
-    constructor() {
-        owner = msg.sender;
-    }
+  modifier restricted() {
+    require(
+      msg.sender == owner,
+      "This function is restricted to the contract's owner"
+    );
+    _;
+  }
 
-    modifier restricted() {
-        if (msg.sender == owner) {
-            _;
-        }
-    }
-
-    function setCompleted(uint256 complete) public restricted {
-        last_completed_migration = complete;
-    }
-
-    function update(address new_address) public restricted {
-        Migrations updaated = Migrations(new_address);
-        updaated.setCompleted(last_completed_migration);
-    }
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
 }
